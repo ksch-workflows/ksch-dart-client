d57f16d9ff3a91ed67662b9520d19fc0e9f8e071
Jan Mewes <jan.mewes@ks-plus.org>
2021-07-03 12:18:27 +0200

    Reorganize test files

--- test/src/patients/patients_resource_test.dart
+++ test/src/patients/patients_resource_test.dart
@@ -3,22 +3,80 @@ import 'package:ksch_dart_client/src/patients/payload.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Should create patient without payload', () async {
-    var client = KschApi('http://localhost:8080');
+  late KschApi api;
 
-    var patient = await client.patients.create();
+  setUp(() {
+    api = KschApi('http://localhost:8080');
+  });
+
+  test('Should create patient without payload', () async {
+    var patient = await api.patients.create();
 
     expect(patient.id, isNotNull);
     expect(patient.links.self.href, startsWith('http://localhost:8080/api/patients/'));
   });
 
   test('Should create patient with payload', () async {
-    var client = KschApi('http://localhost:8080');
-
-    var patient = await client.patients.create(CreatePatientRequestPayload(name: 'John Doe'));
+    var patient = await api.patients.create(CreatePatientRequestPayload(name: 'John Doe'));
 
     expect(patient.id, isNotNull);
     expect(patient.name, isNotNull);
     expect(patient.links.self.href, startsWith('http://localhost:8080/api/patients/'));
   });
+
+  test('should get patient', () async {
+    var createdPatientId = (await api.patients.create()).id;
+
+    var result = await api.patients(createdPatientId).get();
+
+    expect(result.id, equals(createdPatientId));
+  });
+
+  test('should get patient address', () async {
+    var createdPatientId = (await _createPatientJohnDoe(api)).id;
+
+    var result = await api.patients(createdPatientId).residentialAddress.get();
+
+    expect(result.residentialAddress, equals('Guesthouse'));
+  });
+
+  test('should search for patients', () async {
+    var createdPatientId = (await api.patients.create()).id;
+
+    var result = await api.patients.search(createdPatientId);
+
+    expect(result.patients.length, equals(1));
+    expect(result.patients[0].id, equals(createdPatientId));
+    expect(result.page.number, equals(0));
+  });
+
+  test('should URL encode query string in patient search', () async {
+    const patientName = 'John & Doe';
+    await _createPatient(api, patientName);
+
+    var result = await api.patients.search(patientName);
+
+    expect(result.patients.length, greaterThanOrEqualTo(1));
+    expect(result.patients[0].name, equals(patientName));
+  });
+}
+
+Future<PatientResponsePayload> _createPatient(
+    KschApi api,
+    String name,
+    ) async {
+  var payload = CreatePatientRequestPayload(
+    name: name,
+  );
+  return await api.patients.create(payload);
+}
+
+Future<PatientResponsePayload> _createPatientJohnDoe(
+    KschApi api,
+    ) async {
+  var payload = CreatePatientRequestPayload(
+    name: 'John Doe',
+    residentialAddress: 'Guesthouse',
+  );
+  return await api.patients.create(payload);
 }
